ae7e7010ac19ee299b82914ba363a272b0aedd7b
prograTammz <ahmedtamer1998@gmail.com>
2020-05-06 22:40:05 +0200

    added Parse button

--- src/app/parser.ts
+++ src/app/parser.ts
@@ -10,17 +10,21 @@ export class Parser {
     parsingState: boolean;
     error: string
     errorCount: number;
-    currentTokenIndex: number;
+    currentIndex: number;
     constructor(tokens: Token[]){
         this.tokens = tokens;
         this.parsingState = true;
         this.error = "";
         this.errorCount = 0;
-        this.currentTokenIndex = 0;
+        this.currentIndex = 0;
         this.progran();
     }
     private progran(): void{
-
+        console.log("a7a");
+        switch(this.tokens[this.currentIndex].getType){
+            case TokenType.var.toString:
+                console.log("a7a");
+        }
     }
     private block(): void{
 
@@ -75,9 +79,12 @@ export class Parser {
     }
     private addError(token: Token, expectedToken: TokenType):void{
         this.errorCount++;
-        this.error += `ERROR: in line ${token.getLineNumber}. ${expectedToken} Was expected found ${token.getType} instead.`
+        this.error += `ERROR: in line ${token.getLineNumber}. ${expectedToken.toString} Was expected found ${token.getType()} instead.\n`
     }
     public getError():string{
         return this.error;
+    }
+    public getState():boolean{
+        return this.parsingState;
     }
 }
