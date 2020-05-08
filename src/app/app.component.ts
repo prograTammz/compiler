484e9c4e40e2fe35c7b8af60030317e89d6d67cb
prograTammz <ahmedtamer1998@gmail.com>
2020-05-08 11:09:27 +0200

    added tree drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, ViewChild } from '@angular/core';
 import {
   Token
 } from './token';
+import { TreeNode } from './treeNode';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,6 +21,7 @@ export class AppComponent {
   displayedColumns: string[];
   tokenList: Token[];
   isAnalyzed = false;
+  currentTab = 0;
   anaylze(code: string): void{
     //splits the input into separate lines represented as an array of strings.
     let scanner = new Scanner(code);
@@ -33,10 +35,35 @@ export class AppComponent {
     let parser = new Parser(this.tokenList);
     console.log(parser.getError());
     console.log(parser.getState());
-    console.log(parser.getParserTree());
-    parser.createMapping();
+    console.log(parser.getRootNode());
+    this.currentTab = 1;
+    setTimeout(() => {
+      this.processTree(parser.getRootNode(),document.getElementById("tree"));
+    }, 500);
+    
   }
-
+  processTree(node: TreeNode,element){
+    var li = document.createElement('li');
+  li.innerText = node.getName();
+  element.appendChild(li);
+  if (node.children.length) {
+    var ul = document.createElement('ul');
+    li.appendChild(ul);
+    for (var i = 0; i < node.children.length; i++) {
+      this.processTree(node.children[i], ul);
+    }
+  }
+  }
+  constructList(nodes: TreeNode[]): HTMLElement{
+    let list:HTMLElement = document.createElement("UL");
+    for(let node of nodes){
+        let textElm = document.createTextNode(node.getName());
+        let listItem = document.createElement("LI");
+        listItem.appendChild(textElm);
+        list.appendChild(listItem);
+    }
+    return list;
+  }
 }
 
 
